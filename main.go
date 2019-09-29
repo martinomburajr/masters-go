1c1aacd9421a140143a3466ac371d82b5004d706
Martin Ombura <martin.omburajr@gmail.com>
2019-09-29 21:04:03 +0200

    setting up evolutionary engine

--- main.go
+++ main.go
@@ -12,19 +12,28 @@ func main() {
 
 	spec := evolution.Spec2X
 
+	params := evolution.EvolutionParams{
+		EachPopulationSize: 100,
+		AntagonistMaxStrategies: 4,
+		AntagonistStrategyLength: 3,
+		ProtagonistMaxStrategies: 4,
+		ProtagonistStrategyLength: 3,
+		MaxDepth: 3,
+		DepthPenaltyStrategyPenalization: 10,
+	}
+
 	engine := evolution.EvolutionEngine{
 		StartIndividual: evolution.ProgTreeT_NT_T_0,
 		Spec: spec,
 		AvailableStrategies: strategies,
 
 		GenerationCount: 50,
-		EachPopulation: 1,
 
 		AvailableNonTerminalSet: []evolution.SymbolicExpression{evolution.Add, evolution.Mult, evolution.Sub},
 		AvailableTerminalSet: []evolution.SymbolicExpression{
 			evolution.Const1, evolution.Const2, evolution.Const3, evolution.Const4, evolution.Const5, evolution.X1,
 		},
-		MaxDepth: 2,
+
 
 		FitnessStrategy:     evolution.FitnessProtagonistThresholdTally,
 		EvaluationThreshold: 0.1,
@@ -32,6 +41,7 @@ func main() {
 
 		ParentSelection:  evolution.ParentSelectionTournament,
 		TournamentSize:   3,
+		Parameters: params,
 	}
 
 	result, err := engine.Start()
